0929935bc7b85cfe473d471d9b9c3323c04957cd
vijaykumar125 <vijaykumardasari12@gmail.com>
2024-06-23 21:39:06 +0530

    sum

---
+++ sum.java
@@ -0,0 +1,14 @@
+class sum
+{  
+    public static void main(String a[])   
+    {   
+        /*Define variables*/  
+        int x = 8 ;
+        int y = 9 ;
+
+        int c = x + y;   
+        System.out.println("sum of two numbers: " +c );  
+       /*Swapping*/  
+
+    }   
+}  
